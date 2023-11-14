95bedc19e76bc51fa08a54d322828cc19377efab
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2023-11-14 21:30:28 +0000

    tilraun a tengingu. virkadi ekki alveg.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-// ehv rugl bara prufa.
-
 // https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products
+
+// JavaScript code for fetching and displaying products
+fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('products-container');
+    console.log(data);
+    data.items.forEach(product => {
+      const productElement = document.createElement('div');
+
+      productElement.className = 'product-item';
+
+      // Add content to productElement, e.g., product.title, product.price
+      // ...
+      container.appendChild(productElement);
+    });
+  })
+  .catch(error => console.error('Error:', error));// JavaScript code for fetching and displaying products
