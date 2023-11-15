97cdb8ac51e13b60460b6934700ebc58c2ea26f0
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2023-11-15 11:41:14 +0000

    ehv rugl. Allskonar.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,5 @@
-import { empty } from './lib/elements.js';
-import { renderDetails, renderFrontpage, searchAndRender } from './lib/ui.js';
+// index.js
+import { fetchProductData } from "./lib/api.js";
 
-/**
- * Fall sem keyrir við leit.
- * @param {SubmitEvent} e
- * @returns {Promise<void>}
- */
-async function onSearch(e) {
-  e.preventDefault();
-
-  if (!e.target || !(e.target instanceof Element)) {
-    return;
-  }
-
-  const { value } = e.target.querySelector('input') ?? {};
-
-  if (!value) {
-    return;
-  }
-
-  await searchAndRender(document.body, e.target, value);
-  window.history.pushState({}, '', `/?query=${value}`);
-}
-
-/**
- * Athugar hvaða síðu við erum á út frá query-string og birtir.
- * Ef `id` er gefið er stakt geimskot birt, annars er forsíða birt með
- * leitarniðurstöðum ef `query` er gefið.
- */
-function route() {
-  const params = new URLSearchParams(window.location.search);
-  const id = params.get('id');
-  const query = params.get('query');
-
-  if (id) {
-    renderDetails(document.body, id);
-  } else {
-    renderFrontpage(document.body, onSearch, query ?? undefined);
-  }
-}
-
-// Bregst við því þegar við notum vafra til að fara til baka eða áfram.
-window.onpopstate = () => {
-  empty(document.body);
-  route();
-};
-
-// Athugum í byrjun hvað eigi að birta.
-route();
+const url = "https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products";
+fetchProductData(url).then((products) => console.log(products));
