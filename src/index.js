a5a632f19bec6c9ec52b2f7c9961af6a26bca9bd
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2023-11-15 10:55:39 +0000

    synilausn 9 file'ar baett vid

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
-// https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products
+import { empty } from './lib/elements.js';
+import { renderDetails, renderFrontpage, searchAndRender } from './lib/ui.js';
 
-// JavaScript code for fetching and displaying products
-fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('products-container');
-    console.log(data);
-    data.items.forEach(product => {
-      const productElement = document.createElement('div');
+/**
+ * Fall sem keyrir við leit.
+ * @param {SubmitEvent} e
+ * @returns {Promise<void>}
+ */
+async function onSearch(e) {
+  e.preventDefault();
 
-      productElement.className = 'product-item';
+  if (!e.target || !(e.target instanceof Element)) {
+    return;
+  }
 
-      // Add content to productElement, e.g., product.title, product.price
-      // ...
-      container.appendChild(productElement);
-    });
-  })
-  .catch(error => console.error('Error:', error));// JavaScript code for fetching and displaying products
+  const { value } = e.target.querySelector('input') ?? {};
+
+  if (!value) {
+    return;
+  }
+
+  await searchAndRender(document.body, e.target, value);
+  window.history.pushState({}, '', `/?query=${value}`);
+}
+
+/**
+ * Athugar hvaða síðu við erum á út frá query-string og birtir.
+ * Ef `id` er gefið er stakt geimskot birt, annars er forsíða birt með
+ * leitarniðurstöðum ef `query` er gefið.
+ */
+function route() {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get('id');
+  const query = params.get('query');
+
+  if (id) {
+    renderDetails(document.body, id);
+  } else {
+    renderFrontpage(document.body, onSearch, query ?? undefined);
+  }
+}
+
+// Bregst við því þegar við notum vafra til að fara til baka eða áfram.
+window.onpopstate = () => {
+  empty(document.body);
+  route();
+};
+
+// Athugum í byrjun hvað eigi að birta.
+route();
